5b704b9140579458b81967e45273d2c50270e6f3
kikpond <kikpond15@gmail.com>
2022-10-30 11:59:10 +0900

    update

--- python/terrain_leveling.py
+++ python/terrain_leveling.py
@@ -1,13 +1,12 @@
-def leveling(mc, block_id = 2):
+def leveling(mc, block_id=2):
     p = mc.player.getTilePos()
     mc.postToChat("start leveling")
     num = 20
     for i in range(num):
         for j in range(num):
-            mc.setBlock(p.x-(num/2)+i, p.y-1, p.z-(num/2)+j, block_id)        
+            mc.setBlock(p.x-(num/2)+i, p.y-1, p.z-(num/2)+j, block_id)
     mc.postToChat("end leveling")
 
 
 if __name__ == '__main__':
     leveling()
-    
